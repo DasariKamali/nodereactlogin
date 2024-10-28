cf892aad2a39071984e8a1c44c86370d7e502085
DasariKamali <107987424+DasariKamali@users.noreply.github.com>
2024-10-28 12:46:46 +0530

    Update Login.js

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -2,18 +2,8 @@ import React from 'react';
 
 const Login = () => {
   const handleLogin = () => {
-    const params = new URLSearchParams({
-      client_id: process.env.REACT_APP_CLIENT_ID,
-      response_type: 'code',
-      redirect_uri: process.env.REACT_APP_REDIRECT_URI,
-      response_mode: 'query',
-      scope: 'https://graph.microsoft.com/.default offline_access',
-    });
-
-    const loginUrl = `https://login.microsoftonline.com/${process.env.REACT_APP_TENANT_ID}/oauth2/v2.0/authorize?${params.toString()}`;
-    window.location.href = loginUrl;
+    window.location.href = `${process.env.REACT_APP_BACKEND_URL}/api/auth/sso-login`;
   };
-
   return (
     <div>
       <h2>Login Page</h2>
@@ -21,5 +11,4 @@ const Login = () => {
     </div>
   );
 };
-
 export default Login;
